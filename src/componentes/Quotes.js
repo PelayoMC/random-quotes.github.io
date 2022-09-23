a449cb1a61c6433a37e3d9ab4bb34f951e8d94ff
Pelayo Martinez <pmartinezcapela@gmail.com>
2022-09-23 09:45:23 +0200

    acabada app

--- src/componentes/Quotes.js
+++ src/componentes/Quotes.js
@@ -8,37 +8,45 @@ const Quotes = () => {
         quote: "",
         author: "",
     });
+    const [posQuote, setPosQuote] = useState(0);
+
     useEffect(() => {
         setQuote(quotes[0]);
     }, []);
 
     // functions
     const changeQuote = () => {
-        let random = Math.floor(Math.random() * 3);
-        console.log("cambiamos a la quote " + random);
+        let random = Math.floor(Math.random() * quotes.length);
+        while (random === posQuote) {
+            random = Math.floor(Math.random() * quotes.length);
+        }
+        setPosQuote(random);
         setQuote(quotes[random]);
-    };
-    const createTweet = () => {
-        console.log("creamos tweet");
     };
 
     // render
     return (
         <div id="quote-box">
-            <div id="text">{quote.quote}</div>
+            <div id="text">"{quote.quote}</div>
             <div id="author" className="ms-auto me-5">
                 <div>~{quote.author}</div>
             </div>
             <div id="buttons" className="d-flex justify-content-between pt-5">
                 <a
-                    href="/#"
-                    onClick={createTweet}
+                    href={
+                        "https://twitter.com/intent/tweet?text=" +
+                        quote.quote +
+                        " - " +
+                        quote.author
+                    }
+                    target="_blank"
+                    rel="noreferrer"
                     id="tweet-quote"
                     className="me-auto ms-5"
                     data-tip
                     data-for="tweet-quote-toooltip"
                 >
-                    <i className="bi bi-twitter"></i>{" "}
+                    <i className="bi bi-twitter"></i>
                 </a>
                 <ReactTooltip id="tweet-quote-toooltip" place="bottom">
                     Create tweet
